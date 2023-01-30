aea2bb23ac67c339b7f63797bf31f8151c131430
st40404 <lung40404@gmail.com>
2023-01-30 20:43:39 +0800

    add ORBSLAM2 and build build.sh and build_ros.sh, but can't execute all_process

--- docker/config/script/env_setup.sh
+++ docker/config/script/env_setup.sh
@@ -17,6 +17,10 @@ function ENVConfig_fn() {
     SHELL=$(echo ${1} | tr [A-Z] [a-z])
     echo "export TURTLEBOT3_MODEL=waffle" >>/home/$USER/.${SHELL}rc
 
+    PATH = $(echo "$")
+    ## env: set ROS_PACKAGE_PATH
+    echo "export ROS_PACKAGE_PATH=\${ROS_PACKAGE_PATH}:/home/$USER/work/src/ORB_SLAM2/Examples/ROS " >>/home/$USER/.${SHELL}rc
+
     ## env: lidar (permission)
     ##echo "KERNEL=="ttyACM[0-9]*", ACTION=="add", ATTRS{idVendor}=="15d1", MODE="0666", GROUP="dialout", SYMLINK+="sensors/hokuyo"" >> /etc/udev/rules.d/99-hokuyo.rules
     echo "KERNEL=="ttyACM[0-9]*", ACTION=="add", ATTRS{idVendor}=="15d1", MODE="0666", GROUP="dialout", SYMLINK+="sensors/hokuyo"" >> /etc/udev/rules.d/99-hokuyo.rules
